f5dd5e3c065f6ce00eb745dfb45412b5b55dd145
khabib <habib.pakdel1121@gmail.com>
2023-04-20 00:35:08 +0200

    spring initializr

---
+++ back-end/shopeepy/src/main/java/com/example/shopeepy/ShopeepyApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopeepy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeepyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeepyApplication.class, args);
+	}
+
+}
---
+++ back-end/shopeepy/src/test/java/com/example/shopeepy/ShopeepyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopeepy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeepyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
