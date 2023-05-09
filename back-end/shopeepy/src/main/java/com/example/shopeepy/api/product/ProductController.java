4a8e4c6c88023e0c6cd71102d73ebb7307670e16
Kasper Kadestål <84015423+Kasperkadestal@users.noreply.github.com>
2023-05-09 14:21:32 +0200

    Update + Delete | Asynchronous

--- back-end/shopeepy/src/main/java/com/example/shopeepy/api/product/ProductController.java
+++ back-end/shopeepy/src/main/java/com/example/shopeepy/api/product/ProductController.java
@@ -5,7 +5,10 @@ import com.example.shopeepy.model.Product;
 import com.example.shopeepy.service.ProductService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,15 +24,12 @@ public class ProductController {
 
     @GetMapping("/")
     public CompletableFuture<ResponseEntity<List<Product>>> getAvailableProducts() {
-        return productService.getAllProducts().thenApply(product -> ResponseEntity.ok().body(product))
-                .exceptionally(e -> ResponseEntity.notFound().build());
+        return productService.getAllProducts().thenApply(product -> ResponseEntity.ok().body(product)).exceptionally(e -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{id}")
     public CompletableFuture<ResponseEntity<Product>> getProductById(@PathVariable Long id) {
-        return productService.getProductById(id)
-                .thenApply(product -> ResponseEntity.ok().body(product))
-                .exceptionally(e -> ResponseEntity.notFound().build());
+        return productService.getProductById(id).thenApply(product -> ResponseEntity.ok().body(product)).exceptionally(e -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/create")
@@ -37,13 +37,13 @@ public class ProductController {
         return productService.createProduct(product);
     }
 
-    @PutMapping("/{id}")
-    public String updateProduct(@RequestBody Product product) {
-        return "Updated product";
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable String id, @RequestBody Product product) {
+       return productService.updateProduct(id, product);
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteProduct(@RequestHeader Long id) {
-        return "Deleted product ";
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable String id) {
+        return productService.deleteProduct(id);
     }
 }
--- back-end/shopeepy/src/main/java/com/example/shopeepy/service/ProductService.java
+++ back-end/shopeepy/src/main/java/com/example/shopeepy/service/ProductService.java
@@ -3,21 +3,27 @@ package com.example.shopeepy.service;
 import com.example.shopeepy.model.Product;
 import com.google.firebase.database.*;
 import com.google.firebase.internal.NonNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class ProductService {
-
 
     public ProductService() {
         System.out.println("ProductService created");
     }
 
+    /**
+     * Get all products as a list.
+     *
+     * @return List<Product>
+     */
     public CompletableFuture<List<Product>> getAllProducts() {
         CompletableFuture<List<Product>> future = new CompletableFuture<>();
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("product");
@@ -50,6 +56,12 @@ public class ProductService {
         ref.addValueEventListener(listener);
         return future;
     }
+
+    /**
+     * Get product by ID.
+     *
+     * @return Product
+     */
     public CompletableFuture<Product> getProductById(Long productId) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("product").child(String.valueOf(productId));
         CompletableFuture<Product> future = new CompletableFuture<>();
@@ -76,11 +88,36 @@ public class ProductService {
         return future;
     }
 
+    /**
+     * @param product
+     * @return
+     */
     public ResponseEntity<String> createProduct(Product product) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("product");
         DatabaseReference newProductReference = databaseReference.push();
         newProductReference.setValueAsync(product);
         String productId = newProductReference.getKey();
         return ResponseEntity.ok().body(productId);
+    }
+
+    /**
+     * Deletes the product with proposed ID.
+     * @param id
+     * @return ResponseEntity<String>
+     */
+    public ResponseEntity<String> deleteProduct(String id) {
+        try {
+            DatabaseReference productRef = FirebaseDatabase.getInstance().getReference("product").child(id);
+            productRef.removeValueAsync().get();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (InterruptedException | ExecutionException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    public ResponseEntity<String> updateProduct(String id, Product product) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("product/" + id);
+        ref.setValueAsync(product);
+        return ResponseEntity.ok("Product updated successfully");
     }
 }
