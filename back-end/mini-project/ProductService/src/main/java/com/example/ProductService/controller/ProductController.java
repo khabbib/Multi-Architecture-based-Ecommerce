dfedd348afbf4586d3cb1e56738d1ce76d033c3b
khabib <habib.pakdel1121@gmail.com>
2023-05-13 13:46:16 +0200

    update comments

--- back-end/mini-project/ProductService/src/main/java/com/example/ProductService/controller/ProductController.java
+++ back-end/mini-project/ProductService/src/main/java/com/example/ProductService/controller/ProductController.java
@@ -7,49 +7,68 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Controller for products.
+ */
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
 
-    // Get all products
+    /**
+     * Returns all products.
+     * @return ResponseEntity<List<Product>>
+     */
     @GetMapping("/")
-    @ResponseStatus(HttpStatus.OK)
     public  CompletableFuture<ResponseEntity<List<Product>>> getAvailableProducts() {
         return productService.getAllProducts().thenApply(product -> ResponseEntity.ok().body(product))
                 .exceptionally(e -> ResponseEntity.notFound().build());
     }
 
-    // Add product
+    /**
+     * Creates product.
+     * @param product
+     * @return ResponseEntity<String>
+     */
     @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody Product product) {
         productService.createProduct(product);
     }
 
-    // Get product by ID
+    /**
+     * Returns product based on ID.
+     * @param id
+     * @return ResponseEntity<Product>
+     */
     @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public  CompletableFuture<ResponseEntity<Product>> getProductById(@PathVariable("id") String id) throws ExecutionException, InterruptedException {
         return productService.getProductById(id)
                 .thenApply(product -> ResponseEntity.ok().body(product))
                 .exceptionally(e -> ResponseEntity.notFound().build());
     }
 
+    /**
+     * Deletes a product based on ID.
+     * @param id
+     * @return ResponseEntity<String>
+     */
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> deleteProduct(@RequestHeader String id) {
         return productService.deleteProduct(id);
     }
 
+    /**
+     * Updates a product based on ID.
+     * @param id
+     * @param product
+     * @return ResponseEntity<String>
+     */
     @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> updateProduct(@RequestHeader String id, @RequestBody Product product) {
         return productService.updateProduct(id, product);
     }
