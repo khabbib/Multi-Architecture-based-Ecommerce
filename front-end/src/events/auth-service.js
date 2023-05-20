7f38092f4c7cff8ad7655782192c57b69c6d04b2
khabib <habib.pakdel1121@gmail.com>
2023-05-20 14:16:21 +0200

    session storage in auth-service

--- front-end/src/events/auth-service.js
+++ front-end/src/events/auth-service.js
@@ -1,8 +1,32 @@
 
 
 // Login handler
-function login(email, password) {
+async function login(email, password) {
     console.log('login', email, password);
+    // Call the backend API for authentication
+    // If successful, create a session for the user and redirect the user to dashboard
+    // If unsuccessful, redirect the user to the login page with an error message
+
+    const data = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    }).then(res => res.json());
+
+    console.log(data);
+
+    if (data.result === 'success') {
+        // Session
+        sessionStorage.setItem('user', data.user);
+        sessionStorage.setItem('token', data.token);
+        // Redirect
+        window.location.href = '/dashboard';
+    } else {
+        // Show error message
+        document.getElementById('error').innerHTML = data.message;
+    }
     
 }
 
