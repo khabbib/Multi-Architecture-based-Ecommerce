83ba7ed44a5792f8569c8209f8255ee858ddf796
Jakob Hagman <93573152+JSGHagman@users.noreply.github.com>
2023-05-22 13:40:33 +0200

    revert

--- back-end/mini-project/GatewayApplication/src/main/java/com/example/GatewayService/GatewayApplication.java
+++ back-end/mini-project/GatewayApplication/src/main/java/com/example/GatewayService/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.GatewayService;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
