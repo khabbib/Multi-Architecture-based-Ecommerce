703bbc940aa7080bcc955210f82d905839d4b96b
Philip <philip_holmqvist@hotmail.com>
2023-05-24 23:59:39 +0200

    400 bad request error vid inloggning

--- back-end/mini-project/GatewayApplication/src/main/java/com/example/GatewayService/controller/CorsConfig.java
+++ back-end/mini-project/GatewayApplication/src/main/java/com/example/GatewayService/controller/CorsConfig.java
@@ -1,31 +1,23 @@
 package com.example.GatewayService.controller;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.util.pattern.PathPatternParser;
-
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig extends org.springframework.web.cors.CorsConfiguration{
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
+        configuration.setAllowCredentials(false);
+        configuration.setAllowedOrigins(Collections.singletonList("*")); //Eller ändra denna till localhost
         configuration.setAllowedMethods(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true);
-
+        configuration.setAllowedHeaders(Collections.singletonList("content-type"));
         CorsConfigurationSource source = exchange -> configuration;
 
         return new CorsWebFilter(source);
