2a38012039b9f66fa0d430c83de132425d23b39f
khabib <habib.pakdel1121@gmail.com>
2023-05-30 20:28:14 +0200

    wip

--- front-end/src/routes/about/+page.svelte
+++ front-end/src/routes/about/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+	import Table from "../../components/table.svelte";
+</script>
+
 <div class="bg-green-50">
 	<div class="flex items-center justify-center flex-col h-[80vh] gap-10">
 		<h1 class="text-4xl font-bold">About us</h1>
 	</div>
 </div>
+
+
+<Table />
